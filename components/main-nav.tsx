1c392b25d22f68ac92aa00e260a7085d5920fb51
Cillian Barron <barroncillian@gmail.com>
2023-05-21 14:08:11 +0100

    feat: :lipstick: add primary & secondary colours

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -34,10 +34,10 @@ export function MainNav({ items, children }: MainNavProps) {
               key={index}
               href={item.disabled ? "#" : item.href}
               className={cn(
-                "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
+                "flex items-center text-lg font-medium transition-colors hover:text-secondary sm:text-sm",
                 item.href.startsWith(`/${segment}`)
-                  ? "text-foreground"
-                  : "text-foreground/60",
+                  ? "text-secondary"
+                  : "text-primary",
                 item.disabled && "cursor-not-allowed opacity-80"
               )}
             >
