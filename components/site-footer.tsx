fdafd20d9e19c0119c5fb2c0fa8e9b781a5fa9a7
Cillian Barron <barroncillian@gmail.com>
2023-05-28 10:30:28 +0100

    feat: :sparkles: add logo to mobile nav & footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,23 +4,16 @@ import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 import { ModeToggle } from "@/components/mode-toggle"
+import Image from 'next/image'
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logo />
+        <Image src='/images/logo.png' alt='Luas Suas Racing Logo' height={42} width={42}/>
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
-            <a
-              href="https://twitter.com/cillo_b"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              rocinante89
-            </a>
+            Made with ❤️ for Luas Suas Racing
           </p>
         </div>
         <ModeToggle />
